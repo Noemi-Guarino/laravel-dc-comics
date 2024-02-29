6924cd9764983082139adaaa720fa957f6b9cddc
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2024-02-29 18:09:48 +0100

    add table

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -6,37 +6,38 @@
 <div class="container">
     <table class="table">
         <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">title</th>
-            <th scope="col">description</th>
-            <th scope="col">src</th>
-            <th scope="col">price</th>
-            <th scope="col">series</th>
-            <th scope="col">sale_date</th>
-            <th scope="col">type</th>
-            <th scope="col">artists</th>
-            <th scope="col">writers</th>
-          </tr>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">title</th>
+                <th scope="col">description</th>
+                <th scope="col">src</th>
+                <th scope="col">price</th>
+                <th scope="col">series</th>
+                <th scope="col">sale_date</th>
+                <th scope="col">type</th>
+                <th scope="col">artists</th>
+                <th scope="col">writers</th>
+            </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+        @foreach ($comics as $comic)
+            <tr>
+                <th scope="row">{{ $comic->id }}</th>
+                <td>{{ $comic->title }}</td>
+                <td>{{ $comic->description }}</td>
+                <td>{{ $comic->src }}</td>
+                <td>{{ $comic->price }}</td>
+                <td>{{ $comic->series }}</td>
+                <td>{{ $comic->sale_date }}</td>
+                <td>{{ $comic->type }}</td>
+                <td>{{ $comic->artists }}</td>
+                <td>{{ $comic->writers }}</td>
+                <td>
+                    <a href="{{ route('comics.show', ['comic' => $comic->id]) }}" class="btn btn-primary">
+                    Vedi
+                    </a>
+                </td>
+            </tr>
         </tbody>
       </table>
 </div>
