19a2cc5ee6ae8611fce237a690c89f6d9b972d48
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2024-02-29 16:05:06 +0100

    edit route

--- routes/web.php
+++ routes/web.php
@@ -2,52 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Controllers
+use App\Http\Controllers\Admin\ComicController;
+
 
 Route::get('/', function () {
-    $firstName = 'Gino';
-    $lastName = 'Paoli';
-
-    /*
-        compact: crea un array associativo le cui chiavi sono le stringhe
-                 che mettiamo tra le parentesi, mentre i valori di tali
-                 chiavi sono i valori delle variabili con i nomi corrispondenti
-                 alle stringhe inserite
-
-        compact('firstName', 'lastName')
-         |                                     |
-         V                                     V
-
-         [
-            'firstName' => $firstName,
-            'lastName' => $lastName,
-         ]
-    */
-
-    /*
-        dd: vuol dire dump and die, cioè fai il var_dump (più carino però)
-            e poi stoppa l'esecuzione
-    */
-    // dd(compact('firstName', 'lastName'));
-
-    return view('welcome', [
-        'firstName' => $firstName,
-        'lastName' => $lastName,
-    ]);
-    // return view('welcome', compact('firstName', 'lastName'));
-});
-
-Route::get('/chi-siamo', function () {
-    return view('subpages.about');
+    return view('Home');
 });
 
 // Route::get(PERCORSO CON CUI ARRIVARE ALLA PAGINA, FUNZIONE DI CALLBACK CHE MI CREA LA RISPOSTA DA DARE ALL UTENTE)
+
+// crud read
+Route::get('/comics', [ComicController::class, 'index'])->name('Comics.index');
+Route::get('/comics/{id}', [ComicController::class, 'show'])->name('Comics.show');
+
+// crud create
+// Route::get('/comics/create', [ComicController::class, 'create'])->name('pastas.create');
+// Route::post('/comics/add', [ComicController::class, 'store'])->name('pastas.store');
+
+// crud update
+// Route::get('/comics/{id}/update', [PastaController::class, 'edit'])->name('pastas.edit');
+// Route::put('/comics/{request}/{id}/update', [PastaController::class, 'update'])->name('pastas.update');
+
+// crud delete
+// Route::delete('/comics/{id}/delete', [PastaController::class, 'destroy'])->name('pastas.destroy');
